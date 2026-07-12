e4243fc4cb58f31aee669d460aafc116f9c545af
Evgeniy Zuev <Evgeniyzuev43@gmail.com>
2026-07-12 22:09:03 +0300

    Refactor code structure for improved readability and maintainability

--- test/genblox-format.test.js
+++ test/genblox-format.test.js
@@ -1,7 +1,7 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import { parseGenBloxFile, serializeGenBloxFile } from "../js/creator/genblox-format.js";
-import { CLICKER_FILE, CLICKER_GAME, TIC_TAC_TOE_FILE, TIC_TAC_TOE_GAME } from "../js/creator/templates.js";
+import { CLICKER_FILE, CLICKER_GAME, RUNNER_FILE, RUNNER_GAME, TIC_TAC_TOE_FILE, TIC_TAC_TOE_GAME } from "../js/creator/templates.js";
 
 test("Clicker template round-trips",()=>assert.deepEqual(parseGenBloxFile(serializeGenBloxFile(CLICKER_GAME)),CLICKER_GAME));
 test("exported Clicker parses",()=>assert.equal(parseGenBloxFile(CLICKER_FILE).manifest.templateId,"clicker-1"));
@@ -10,3 +10,4 @@ test("rejects Markdown fences",()=>assert.throws(()=>parseGenBloxFile(`\`\`\`\n$
 test("rejects broken JSON",()=>assert.throws(()=>parseGenBloxFile(CLICKER_FILE.replace('"formatVersion": 1','"formatVersion":')),/MANIFEST JSON is broken/));
 test("rejects scripts hidden in HTML",()=>assert.throws(()=>parseGenBloxFile(CLICKER_FILE.replace('<main class="game">','<script>alert(1)</script><main class="game">')),/HTML section contains a script tag/));
 test("Tic-Tac-Toe remix round-trips",()=>assert.deepEqual(parseGenBloxFile(TIC_TAC_TOE_FILE),TIC_TAC_TOE_GAME));
+test("Runner remix round-trips",()=>assert.deepEqual(parseGenBloxFile(RUNNER_FILE),RUNNER_GAME));
